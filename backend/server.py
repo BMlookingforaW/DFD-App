48ea91bdb3d54ee323f80ca5657e17538d63e00b
Bharat Mishra <bharatmishrarex@gmail.com>
2025-05-18 13:25:45 +0530

    logs

--- backend/server.py
+++ backend/server.py
@@ -91,5 +91,7 @@ def ping():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
+    print(f"🚀 Binding to port {port}...")
+    print("🌐 Flask successfully running!")
     app.run(host="0.0.0.0", port=port)
 
